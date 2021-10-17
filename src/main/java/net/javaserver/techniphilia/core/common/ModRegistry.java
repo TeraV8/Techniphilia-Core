723c305cc2c9c6576d142edf360157bfba2e425d
TeraV <vharris1800@gmail.com>
2021-10-17 14:54:44 -0400

    Added copper/tin/aluminum tool types

--- src/main/java/net/javaserver/techniphilia/core/common/ModRegistry.java
+++ src/main/java/net/javaserver/techniphilia/core/common/ModRegistry.java
@@ -3,12 +3,17 @@ package net.javaserver.techniphilia.core.common;
 import net.javaserver.techniphilia.core.common.block.BlockCopperOre;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTool;
+import net.minecraftforge.common.util.EnumHelper;
 import org.apache.logging.log4j.Logger;
 
 public class ModRegistry {
     public static final String MOD_NAME = "Techniphilia Core";
     public static final String MOD_ID = "techniphilia-core";
     public static final String MOD_VERSION = "0.5.11.9";
+    public static final ItemTool.ToolMaterial COPPER_TOOL = EnumHelper.addToolMaterial("COPPER", 1, 197, 5.0F, 1.6F, 8);
+    public static final ItemTool.ToolMaterial TIN_TOOL = EnumHelper.addToolMaterial("TIN", 1, 164, 5.6F, 1.8F, 9);
+    public static final ItemTool.ToolMaterial ALUMINUM_TOOL = EnumHelper.addToolMaterial("ALUMINUM", 2, 387, 6.5F, 2.4F, 16);
     public static Logger logger;
     private static ModItemGroup itemGroup = null;
     private ModRegistry() {}
