2ed44e793753ed2bb4c6845f789789b9c820ffe8
TeraV <vharris1800@gmail.com>
2021-10-27 22:08:21 -0400

    Increased hammer attack damage

--- src/main/java/net/javaserver/techniphilia/core/common/tool/ToolHammer.java
+++ src/main/java/net/javaserver/techniphilia/core/common/tool/ToolHammer.java
@@ -5,6 +5,6 @@ import net.minecraft.item.ItemTool;
 public class ToolHammer extends ItemTool {
     // tool stuff
     ToolHammer(ToolMaterial m) {
-        super(0.5F, -3.2F, m, null);
+        super(2.5F, -3.2F, m, null);
     }
 }
