895b90605330c1dd4f42a0049a3f12c187f45ff9
TeraV <vharris1800@gmail.com>
2021-11-27 12:44:46 -0500

    Added an empty set to satisfy blocks minable by hammer

--- src/main/java/net/javaserver/techniphilia/core/common/tool/ToolHammer.java
+++ src/main/java/net/javaserver/techniphilia/core/common/tool/ToolHammer.java
@@ -1,10 +1,11 @@
 package net.javaserver.techniphilia.core.common.tool;
 
+import java.util.Collections;
 import net.minecraft.item.ItemTool;
 
 public class ToolHammer extends ItemTool {
     // tool stuff
     ToolHammer(ToolMaterial m) {
-        super(2.5F, -3.2F, m, null);
+        super(2.5F, -3.2F, m, Collections.EMPTY_SET);
     }
 }
