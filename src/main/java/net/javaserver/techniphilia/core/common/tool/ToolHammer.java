07c46346e2d848c527ab3bcf8a4a533799548347
TeraV <vharris1800@gmail.com>
2022-06-08 18:49:53 -0400

    Tweaked hammer damage Added version 1.0.1.4 information

--- src/main/java/net/javaserver/techniphilia/core/common/tool/ToolHammer.java
+++ src/main/java/net/javaserver/techniphilia/core/common/tool/ToolHammer.java
@@ -6,6 +6,6 @@ import net.minecraft.item.ItemTool;
 public class ToolHammer extends ItemTool {
     // tool stuff
     ToolHammer(ToolMaterial m) {
-        super(2.5F, -3.2F, m, Collections.EMPTY_SET);
+        super(3.8F, -3.2F, m, Collections.EMPTY_SET);
     }
 }
